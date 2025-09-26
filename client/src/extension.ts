7896ec5db68d203b4d159227524e56d20de144df
uxiun <uxiuns@gmail.com>
2025-09-27 00:22:53 +0900

    biome format

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,23 +3,23 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import * as path from "path";
-import { workspace, ExtensionContext } from "vscode";
+import * as path from "path"
+import { workspace, ExtensionContext } from "vscode"
 
 import {
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
-	TransportKind,
-} from "vscode-languageclient/node";
+	TransportKind
+} from "vscode-languageclient/node"
 
-let client: LanguageClient;
+let client: LanguageClient
 
 export function activate(context: ExtensionContext) {
 	// The server is implemented in node
 	const serverModule = context.asAbsolutePath(
-		path.join("server", "out", "server.js"),
-	);
+		path.join("server", "out", "server.js")
+	)
 
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
@@ -27,9 +27,9 @@ export function activate(context: ExtensionContext) {
 		run: { module: serverModule, transport: TransportKind.ipc },
 		debug: {
 			module: serverModule,
-			transport: TransportKind.ipc,
-		},
-	};
+			transport: TransportKind.ipc
+		}
+	}
 
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
@@ -37,25 +37,25 @@ export function activate(context: ExtensionContext) {
 		documentSelector: [{ scheme: "file", language: "plaintext" }],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-		},
-	};
+			fileEvents: workspace.createFileSystemWatcher("**/.clientrc")
+		}
+	}
 
 	// Create the language client and start the client.
 	client = new LanguageClient(
 		"languageServerExample",
 		"Language Server Example",
 		serverOptions,
-		clientOptions,
-	);
+		clientOptions
+	)
 
 	// Start the client. This will also launch the server
-	client.start();
+	client.start()
 }
 
 export function deactivate(): Thenable<void> | undefined {
 	if (!client) {
-		return undefined;
+		return undefined
 	}
-	return client.stop();
+	return client.stop()
 }
